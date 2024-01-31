4a04186296a82900492ef5b8c9d7233b45285f84
mrunali salunke <salunkemrunali@gmail.com>
2024-01-31 10:12:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Game from './Components/Game'
+import Scrolling from './Components/Scrolling'
+import './App.css';
+
+
+function App() {
+  return (
+    
+    <div className="App">
+    <Game/>
+    </div>
+    
+  );
+}
+
+export default App;
